cd99722dac277675dc25f831606a596aae5a8a22
MAGICLICK\uokcelik <uokcelik@MagiClick.com>
2023-08-08 17:15:01 +0300

    -

--- pages/abstract/main.js
+++ pages/abstract/main.js
@@ -42,8 +42,6 @@ const content = [
 ];
 
 const generateJson = () => {
-  var main_content = document.getElementsByClassName("content-obj");
-
   content.map((item) => {
     $(".content-obj").append(`
     <div class="col-md-6 content-list-each">
