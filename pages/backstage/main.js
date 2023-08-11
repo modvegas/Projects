49cbd2a9d5caab76e286a77b96bb11fd517b7de6
MAGICLICK\uokcelik <uokcelik@MagiClick.com>
2023-08-11 18:03:02 +0300

    boş komit

--- pages/backstage/main.js
+++ pages/backstage/main.js
@@ -1,17 +1,21 @@
-$(document).ready(function () {
-  $("a").on("click", function (event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $("body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        1000,
-        function () {
-          window.location.hash = hash;
-        }
-      );
-    }
-  });
-});
+// tıklanan a elementiyle ilişkilendirilen elemnti bul.
+
+// $(document).ready(function () {
+//   $("a").on("click", function (event) {
+//     if (this.hash !== "") {
+//       event.preventDefault();
+//       var hash = this.hash;
+//       debugger;
+//       $("html,body").animate(
+//         {
+//           scrollTop: $(hash).offset().top,
+//         },
+//         1000,
+//         function () {
+//           debugger;
+//           window.location.hash = hash;
+//         }
+//       );
+//     }
+//   });
+// });
