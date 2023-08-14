8cc09dd4660d015603eae8b82c7390d8f49b75ea
MAGICLICK\uokcelik <uokcelik@MagiClick.com>
2023-08-14 09:29:44 +0300

    js uncommented

--- pages/backstage/main.js
+++ pages/backstage/main.js
@@ -1,21 +1,19 @@
-// tıklanan a elementiyle ilişkilendirilen elemnti bul.
-
-// $(document).ready(function () {
-//   $("a").on("click", function (event) {
-//     if (this.hash !== "") {
-//       event.preventDefault();
-//       var hash = this.hash;
-//       debugger;
-//       $("html,body").animate(
-//         {
-//           scrollTop: $(hash).offset().top,
-//         },
-//         1000,
-//         function () {
-//           debugger;
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-// });
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      debugger;
+      $("html,body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1000,
+        function () {
+          debugger;
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
