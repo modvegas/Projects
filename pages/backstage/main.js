3da75b68feff3884808a3c5f1df1f9b3d8f033a5
MAGICLICK\uokcelik <uokcelik@MagiClick.com>
2023-08-14 15:24:49 +0300

    scroll problems

--- pages/backstage/main.js
+++ pages/backstage/main.js
@@ -1,17 +1,86 @@
 $(document).ready(function () {
   $("a").on("click", function (event) {
+    var href = $(this).attr("href");
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
+      var issueNumber = hash.slice(-1);
       $("html,body").animate(
         {
           scrollTop: $(hash).offset().top,
         },
         800,
         function () {
-          window.location.hash = hash;
+          window.location.hash = href;
+          $("body").css("background-color", colors[issueNumber - 1]);
         }
       );
     }
+  });
+  function loop() {
+    $(".page").bind("mousewheel", function (e) {
+      var hash = window.location.hash;
+      hash = hash.startsWith("#") ? hash.substring(1) : hash;
+      var issueNumber = hash.slice(-1);
+      e.preventDefault();
+      window.location.hash = hash;
+      if (e.originalEvent.wheelDelta / 120 > 0) {
+        $("html,body").animate(
+          {
+            scrollTop: $("#issue" + ("issueNumber" + 1).toString()).offset()
+              .top,
+          },
+          300,
+          function () {
+            $("body").css("background-color", colors[issueNumber - 1]);
+          }
+        );
+        console.log(issueNumber);
+      } else {
+        $("html,body").animate(
+          {
+            scrollTop: $("#issue" + (issueNumber - 1).toString()).offset().top,
+          },
+          300,
+          function () {
+            $("body").css("background-color", colors[issueNumber - 1]);
+          }
+        );
+        console.log(issueNumber);
+      }
+    });
+  }
+  loop();
+  $.fn.isInViewport = function () {
+    let elementTop = $(this).offset().top;
+    let elementBottom = elementTop + $(this).outerHeight();
+
+    let viewportTop = $(window).scrollTop();
+    let viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
+});
+var colors = ["red", "blue", "magenta", "green"];
+
+// var colorsObj = {issue1:'red',issue2:'blue'};
+
+const section = document.querySelector(".pages");
+const activities = section.querySelectorAll(".page");
+
+activities.forEach((activityElement, i) => {
+  activityElement.addEventListener("scroll", () => {
+    if (activityElement.classList.contains("active")) return;
+
+    // Animations to previous/next element
+    const previousActiveElement = section.querySelector(".active");
+
+    previousActiveElement.classList.remove("active");
+    activityElement.classList.add("active");
+
+    // Background
+    const item = activityElement.getAttribute("data-item");
+    const wave = document.querySelector(`[data-wave="${item}"]`);
+    wave.parentNode.appendChild(wave);
   });
 });
