832a2e5da57566b66acc6c6903a3220bd6a1caa7
MAGICLICK\uokcelik <uokcelik@MagiClick.com>
2023-08-15 15:04:40 +0300

    fixed delays and scroll behaviour

--- pages/backstage/main.js
+++ pages/backstage/main.js
@@ -1,26 +1,6 @@
-$(document).ready(function () {
-  $("a").on("click", function (event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $("html,body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        500,
-        function () {
-          window.location.hash = hash;
-        }
-      );
-    }
-  });
-});
-
-// var classList = ["red", "blue", "yellow", "orange", "teal", "white", "pink"];
-
 const colors = [
   "ff608c",
-  "#fff",
+  "#ddd",
   "#00c1b5",
   "#ff6519",
   "#ffbe00",
@@ -28,32 +8,28 @@ const colors = [
   "#e30512",
 ];
 function scrollDetect() {
-  var lastScroll = 0;
+  let lastScroll = 0;
   let counter = 0;
-  var jumpDiv = "issue" + counter;
-  var divInView = document.getElementById(jumpDiv);
   window.onscroll = function () {
     let currentScroll =
       document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
     if (currentScroll > 0 && lastScroll <= currentScroll) {
       lastScroll = currentScroll;
+      document.getElementById("body").style.background =
+        colors[Math.floor(Math.random() * 7)];
       counter++;
-      document.getElementById("body").style.background = colors[counter];
 
       if (counter > 6) {
         return (counter = 6);
       }
-      console.log(jumpDiv);
-      console.log(counter);
     } else {
       lastScroll = currentScroll;
+      document.getElementById("body").style.background =
+        colors[Math.floor(Math.random() * 7)];
       counter--;
-      document.getElementById("body").style.background = colors[counter];
       if (counter < 0) {
         return (counter = 0);
       }
-      divInView.scrollIntoView({});
-      console.log(counter);
     }
   };
 }
