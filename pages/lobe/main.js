e4f8323d84f14fe1589a42764de54441fbb0faa6
modvegas <uokcelik@MagiClick.com>
2023-08-18 15:39:26 +0300

    post page added blog page objected

--- pages/lobe/main.js
+++ pages/lobe/main.js
@@ -1,3 +1,55 @@
+const blogContent = [
+  {
+    img: "images/post1.png",
+    title: "Use your model in Power Platform",
+    description:
+      "We're excited to announce a new integration with Microsoft Power Platform.",
+    url: "#",
+  },
+  {
+    img: "images/post2.jpg",
+    title: "Introducing new ways to use your model",
+    description:
+      "Lobe 0.9 introduces new ways of using and exporting your model to your app.",
+    url: "#",
+  },
+  {
+    img: "images/post3.jpg",
+    title: "Using your model 101",
+    description:
+      "Learn everything you need to know to export your model and use it in your app.",
+    url: "#",
+  },
+  {
+    img: "images/post4.jpg",
+    title: "Machine Learning Kit with Adafruit",
+    description:
+      "Introducing a brand new machine learning kit from Adafruit and Lobe.",
+    url: "#",
+  },
+  {
+    img: "images/post5.jpg",
+    title: "Four New Features in Lobe",
+    description:
+      "Select your camera source, export your model to new formats, and more in the new Lobe.",
+    url: "#",
+  },
+  {
+    img: "images/post6.jpg",
+    title: "Endless Camera Sources",
+    description:
+      "Deep dive into the new camera sources and all the possibilities they open up inside of Lobe.",
+    url: "#",
+  },
+  {
+    img: "images/post7.jpg",
+    title: "Machine Learning Made Easy",
+    description:
+      "Everything you need to train custom machine learning models in a free, easy to use app.",
+    url: "#",
+  },
+];
+
 const footerLinks = [
   {
     title: "Abot",
@@ -22,8 +74,29 @@ function activePage() {
     $(".tourpage").addClass("active");
   } else if (last == "blog.html") {
     $(".blogpage").addClass("active");
+    loadBlog();
   }
 }
+function loadBlog() {
+  blogContent.map((item) => {
+    $(".blog-entries").append(`
+    <a href="${item.url}" class="blog-card">
+        <div class="blog-content-image">
+            <div class="blog-content-image-container">
+                <img src="${item.img}" alt="Blog Post" />
+            </div>
+        </div>
+        <div class="blog-content-card">
+                <div class="card-title">${item.title}</div>
+            <div class="card-text">
+                ${item.description}
+            </div>
+            <div class="card-footer">Release</div>
+        </div>
+    </a>`);
+  });
+}
+
 window.onload = () => {
   activePage();
   $(".last-content").append(`
