ae1fe456cab368753a340ca287e1ee5aa00022ae
modvegas <uokcelik@MagiClick.com>
2023-08-21 13:38:09 +0300

    feat: examples page videos added

--- pages/lobe/main.js
+++ pages/lobe/main.js
@@ -55,97 +55,113 @@ const exampleContent = [
     img: "images/examples/plants.jpg",
     title: "Plant Species",
     description:
-      "We're excited to announce a new integration with Microsoft Power Platform.",
+      "Train your app to distinguish between different species of plants to help people identify poisonous vegetation.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/hand.jpg",
     title: "Hand Gestures",
     description:
-      "Lobe 0.9 introduces new ways of using and exporting your model to your app.",
+      "Train your app to understand the movement of your hand to create new kinds of immersive user experiences.",
+    video: "images/examples/hand.mp4",
   },
   {
     img: "images/examples/burn.jpg",
     title: "Personal Trainer",
     description:
-      "Learn everything you need to know to export your model and use it in your app.",
+      "Train your app to recognize different workout positions to create an automated personal trainer that counts your reps.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/laughing.jpg",
     title: "Emotional Reactions",
     description:
-      "Introducing a brand new machine learning kit from Adafruit and Lobe.",
+      "Train your app to react to different expressions allowing people to send emoji reactions using just their face.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/painting.jpg",
     title: "Interactive Painting",
     description:
-      "Select your camera source, export your model to new formats, and more in the new Lobe.",
+      "Train your app to respond with sounds and stories while kids draw to create engaging learning experiences.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/mask.jpg",
     title: "Safety Precautions",
     description:
-      "Deep dive into the new camera sources and all the possibilities they open up inside of Lobe.",
+      "Train your app to monitor when masks are being correctly worn to help encourage proper safety precautions.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/whales.jpg",
     title: "Whale Watching",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to detect marine life in tourist photos to help conservationists map and protect the ocean.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/bees.jpg",
     title: "Beehive Health",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to monitor the entrance of hives to help beekeepers track and analyze the health of their colonies.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/sleep.jpg",
     title: "Baby Monitor",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to detect when babies are sleeping or awake to notify parents before they start crying.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/check.jpg",
     title: "Smart Checkout",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to recognize different types of produce to help checkout faster without the need to remember price look-up codes.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/wild.jpg",
     title: "Fire Watch",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to detect smoke and flames using remote cameras to help spot new wildfires before it's too late.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/micro.jpg",
     title: "Scientific Research",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to detect organisms under a microscope to help researchers analyze large quantities of scientific samples.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/elephant.jpg",
     title: "Wildlife Behavior",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to detect wildlife behavior with remote cameras to track population and migration patterns over a large area.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/compress.jpg",
     title: "Telescopic Imagery",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to monitor analog gauges and equipment to provide modern digital readouts and real time analytics.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/telescope.jpg",
     title: "Equipment Analytics",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to recognize different celestial bodies to help astronomers analyze large amounts of telescopic imagery.",
+    video: "images/examples/plants.mp4",
   },
   {
     img: "images/examples/deforest.jpg",
     title: "Aerial Imagery",
     description:
-      "Everything you need to train custom machine learning models in a free, easy to use app.",
+      "Train your app to monitor aerial imagery to gain insights into macro level changes happening around the globe.",
+    video: "images/examples/plants.mp4",
   },
 ];
 
@@ -203,9 +219,12 @@ function loadExamples() {
   exampleContent.map((item) => {
     $(".example-posts").append(`
         <div class="example-card">
-          <div class="blog-content-image">
+          <div class="example-content-image">
             <div class="blog-content-image-container">
               <img src="${item.img}" alt="Blog Post" />
+              <video class="" playsinline="" webkit-playsinline="true" loop="" preload="none" title="Video of the ${item.title}" style="visibility: visible">
+                <source src="${item.video}" type="video/mp4" />
+              </video>
             </div>
           </div>
           <div class="blog-content-card">
